02d3f0c8d0bee6a66053471060164552660b6891
Felix <jonfelixrico@gmail.com>
2022-07-15 00:38:02 +0800

    Push code generated by STS4

---
+++ src/main/java/com/wisdom/WisdomCoreApplication.java
@@ -0,0 +1,13 @@
+package com.wisdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisdomCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisdomCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisdom/WisdomCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisdomCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
