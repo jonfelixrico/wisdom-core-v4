a971dcf7c8ec306a0a9ecdb5ea009b75ea3dd272
Felix <jonfelixrico@gmail.com>
2022-07-15 01:02:04 +0800

    Create receive endpoint

--- src/main/java/com/wisdom/controller/quote/QuoteController.java
+++ src/main/java/com/wisdom/controller/quote/QuoteController.java
@@ -3,7 +3,10 @@
  */
 package com.wisdom.controller.quote;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,8 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class QuoteController {
-	@RequestMapping("/quotes/random")
-	QuoteRespDto getRandomQuote() throws Exception {
+	@GetMapping("/guild/{guildId}/quotes/random")
+	QuoteRespDto getRandomQuote(@PathVariable String guildId) throws Exception {
+		throw new Exception("noop");
+	}
+	
+	@PostMapping("/guild/{guildId}/quotes/{quoteId}/receives")
+	void receiveQuote(@RequestBody QuoteReceiveReqDto body) throws Exception {
 		throw new Exception("noop");
 	}
 }
---
+++ src/main/java/com/wisdom/controller/quote/QuoteReceiveReqDto.java
@@ -0,0 +1,43 @@
+package com.wisdom.controller.quote;
+
+import java.time.Instant;
+
+/**
+ * 
+ * @author Felix
+ *
+ */
+public class QuoteReceiveReqDto {
+	/**
+	 * Id of the user who received the quote
+	 */
+	private String receivingUserId;
+	
+	/**
+	 * Id of the channel where the receive happened
+	 */
+	private String receivingChannelId;
+	
+	/**
+	 * The timestamp where the quote got received
+	 */
+	private Instant receiveDt;
+	
+	public QuoteReceiveReqDto(String receivingUserId, String receivingChannelId, Instant receiveDt) {
+		this.receivingUserId = receivingUserId;
+		this.receivingChannelId = receivingChannelId;
+		this.receiveDt = receiveDt;
+	}
+
+	public String getReceivingUserId() {
+		return receivingUserId;
+	}
+
+	public Instant getReceiveDt() {
+		return receiveDt;
+	}
+
+	public String getReceivingChannelId() {
+		return receivingChannelId;
+	}	
+}
